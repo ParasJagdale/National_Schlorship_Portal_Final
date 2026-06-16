a6fbb49d2f829230f32071cae0ea1dfd23af9dcc
Gangadhar Jagdale <2488261@cognizant.com>
2026-06-16 10:57:03 +0530

    Seperated the Routes according to Roles

---
+++ Backend/src/routes/institute.js
@@ -0,0 +1,19 @@
+import { Router } from "express";
+import { requireStudentAuth } from "../middleware/auth.js";
+
+import {
+  registerInstitute,
+  loginInstitute,
+  getInstituteNameByCode,
+} from "../controllers/instituteController.js";
+
+export const instituteRouter = Router();
+
+// --- Institute Controller Routes ---
+instituteRouter.post("/register/institute", registerInstitute);
+instituteRouter.post("/institute/login", loginInstitute);
+instituteRouter.get(
+  "/institute/info/:code",
+  requireStudentAuth,
+  getInstituteNameByCode,
+);
